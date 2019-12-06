f2210945c49e8cb59cb0558b8ed72bc08eb2e1d7
jiyoungdemadre <55229437+jiyoungdemadre@users.noreply.github.com>
2019-12-06 12:12:49 +0100

    views

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -5,6 +5,49 @@
 
 @section('content')
 
-<p>Some content comes here in the future</p>
+<section id="home">
+    <h1><span>@</span>yourService</a></h1>
+    <h2>Linking expats to experts in Luxembourg</h2>
+    <div class="search-container">
+        <form action=""> <!--/action_page.php-->
+            <input type="text" placeholder="Search your service..." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+    </div>
+</section>
+
+<div class="card-deck">
+    <div class="card">
+        <img class="card-img-top" src="https://randomuser.me/api/portraits/women/74.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Service Provider 1</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+        <button type="button" class="btn btn-outline-danger">Details</button>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="https://randomuser.me/api/portraits/men/79.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Service Provider 2</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer">
+        <button type="button" class="btn btn-outline-danger">Details</button>
+        </div>
+    </div>
+    <div class="card">
+            <img class="card-img-top" src="https://randomuser.me/api/portraits/men/85.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Service Provider 3</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-outline-danger">Details</button>
+
+        </div>
+    </div>
+</div>
 
 @endsection
--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -4,19 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
     <title>@yield('title')</title>
 </head>
 <body>
-    <nav>
-        blablabla
-        @if(!Auth::user())
-        <a href="/register">Register</a>
-        <a href="/login">Login</a>
-        @endif
-        @if(Auth::user())
-        <a href="/logout">Logout</a>
-        @endif
-    </nav>
+    <header>
+        <h1><a href="#home"><span>@</span>yourService </a><img src="../atYourService/style/images/waiter (1).png" alt=""></h1>
+        <nav>
+            <ul>
+                @if(!Auth::user())
+                <li><a href="#aboutus">About us</a></li>
+                <li><a href="#contact">Contact us</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+                @endif
+                @if(Auth::user())
+                <a href="/logout">Logout</a>
+                @endif
+            </ul>
+        </nav>
+    </header>
 
     <div class="content">
         @yield('content')
