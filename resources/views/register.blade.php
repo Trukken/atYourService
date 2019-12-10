5993aeb6c0453f3fcd898ac499885216bd609652
Trukken <tomolya12@gmail.com>
2019-12-10 11:25:46 +0100

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -27,8 +27,11 @@
     <input type="hidden" name="token" id="token">
     <input type="submit" name="submit" value="Submit">
 </form>
+
+
         <script src="https://www.google.com/recaptcha/api.js?render=6LeqRcYUAAAAAC6bqp95JOb30MzDlY1gskQng9kL"></script>
         <script>
+            //
         grecaptcha.ready(function() {
             grecaptcha.execute('6LeqRcYUAAAAAC6bqp95JOb30MzDlY1gskQng9kL', {action: 'login'}).then(function(token) {
                 document.querySelector('#token').value = token;
