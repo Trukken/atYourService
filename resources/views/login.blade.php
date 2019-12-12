caf11bd39bc95e4c25bbc468e856d92aecce9f75
Trukken <tomolya12@gmail.com>
2019-12-12 13:02:06 +0100

    added error display

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,15 @@
 
 @section('content')
 
-
+@if($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 
 @if(isset($loginError))
     <?php echo $loginError; ?>
