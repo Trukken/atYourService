2ba2fd3b0e720d946c84078fdae8d1900d7a74d1
fernandaalvim <fernandaalvim@users.noreply.github.com>
2019-12-16 10:18:00 +0100

    changes to my account and added edit user

---
+++ resources/views/edit-user.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.container')
+​
+@section('title','Edit {{ $user->name }}')
+​
+@section('content')
+​
+<h2>Update your info</h2>
+​
+<form id="form" method="POST">
+    @csrf
+    @method('PUT')
+    <label for="name">Name:</label>
+    <br>
+    <input type="text" class="inner editing-user" name="name" value="{{$user->name}}" id="">
+    <br>
+    <label for="phone">Phone number:</label>
+    <br>
+    <input name="phone_number" class="inner editing-user" value="{{$user->phone_number}}">
+    <br>
+    <label for="image">profile picture url:</label>
+    <br>
+    <input name="image" class="inner editing-user" value="{{$user->image}}">
+    <br>
+    <button class="btn peach-gradient btn-rounded btn-sm my-0 waves-effect waves-light" type="submit">Edit</button>
+    ​
+    <!-- <input type="submit" name="edit" value="edit"> -->
+    ​
+</form>
+​
+<div id="resultForm"></div>
+<br>
+<a href="/user/{{ auth()->user()->id }}">Back to My Account</a>
+​
+​
+​
+​
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script>
+    $(function() {
+        $('button[type="submit"]').click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: '/user/edit/{{$user->id}}',
+                type: 'put',
+                data: $('#form').serialize(),
+                success: function(result) {
+                    console.log(result);
+                    console.log('success');
+                    $('#resultForm').html('<div>' + result + '</div>');
+                },
+                error: function(err) {
+                    // Si une erreur AJAX se produit
+                    console.log('error');
+                }
+            });
+        });
+    });
+</script>
+@endsection
--- resources/views/myaccount.blade.php
+++ resources/views/myaccount.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.container')
-
+​
 @section('title','My account')
-
+​
 @section('content')
-
-
-<h1>My services:</h1>
-
-
+​
+​
+<h1>My Account</h1>
+<br>
+​
+​
 @if(Auth::user() && Auth::user()->admin == true)
 <form action="/user-control" method="POST">
     @csrf
@@ -19,31 +20,54 @@
     @endif
 </form>
 @endif
-@foreach($user->services as $service)
-<br>
-<h3>{{$service->name}}</h3>
-
-<a class="read-more-toggle">Read More <i class="fas fa-angle-down"></i></a>
-<div class="read-more-content">
-    <br>
-    <h5>Short description:</h5>
-    <p> {{$service->short_description}}</p>
-
-    <h5>Complete description:</h5>
-    <p> {{$service->long_description}}</p>
-
-    <h5>Date created:</h5>
-    <p>{{date('d.m.Y', strtotime($service->created_at))}}</p>
-
+​
+​
+​
+<div class="user-page">
+    <div class="user-profile">
+        <h2>Profile</h2>
+        <div class="user-details">
+            <div class="user-picture">
+                <img src="{{$user->image}}" alt="profile picture">
+            </div>
+            <div class="user-info">
+                <p><strong>Name: </strong> {{ $user->name }} </p>
+                <p><strong>E-mail: </strong> {{ $user->email }} </p>
+                <p><strong>Phone number: </strong> {{ $user->phone_number }} </p>
+            </div>
+        </div>
+        <a href="/user/edit/{{auth()->user()->id}}" class="btn peach-gradient btn-rounded btn-sm my-0 waves-effect waves-light" type="submit">Edit</a>
+    </div>
+    ​
+    <div class="user-services">
+        <h2>Provided Services:</h2>
+        @foreach($user->services as $service)
+        <br>
+        <h3>{{$service->name}}</h3>
+        ​
+        <a class="read-more-toggle">Read More <i class="fas fa-angle-down"></i></a>
+        <div class="read-more-content">
+            <br>
+            <h5>Short description:</h5>
+            <p> {{$service->short_description}}</p>
+            ​
+            <h5>Complete description:</h5>
+            <p> {{$service->long_description}}</p>
+            ​
+            <h5>Date created:</h5>
+            <p>{{date('d.m.Y', strtotime($service->created_at))}}</p>
+            ​
+        </div>
+        ​
+        ​
+        @if(Auth::user() && Auth::user()->id == $user->id)
+        <p>(<a href="/services/edit/{{$service->id}}">Update</a>/
+            <a id="delete" href="/services/delete/{{$service->id}}">Delete</a>)</p>
+        ​
+        @endif
+        @endforeach
+    </div>
 </div>
-
-
-@if(Auth::user() && Auth::user()->id == $user->id)
-<p>(<a href="/services/edit/{{$service->id}}">Update</a>/
-    <a id="delete" href="/services/delete/{{$service->id}}">Delete</a>)</p>
-
-@endif
-@endforeach
 <div class="result"></div>
 
 
